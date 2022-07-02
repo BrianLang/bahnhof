cf482ee8822c797fb3428fe97ced3ce60e1672f6
BrianLang <brianlang89@gmail.com>
2022-07-02 11:09:59 +0200

    update git cleanup add messages switch to main (user supplied) show local and remote branches delete local branches that are not on remote (and are not developed). document new argument

--- R/git_utils.R
+++ R/git_utils.R
@@ -3,13 +3,34 @@
 #' Prunes branches (removes them from list of remote branches) from your local repo that are removed in the remote (origin).
 #' Deletes branches locally that have status "merged" (removes them from list of local branches).
 #'
+#' @param main_branch name of the master branch
+#'
 #' @examples
 #' \dontrun{
 #' git_cleanup_local()
 #' }
 #'
 #' @export
-git_cleanup_local <- function() {
+git_cleanup_local <- function(main_branch = "main") {
+  message("\nSwitching to main branch:")
+  system(paste0("git checkout ", main_branch))
+  Sys.sleep(.0005)
+  # drop branches from remote list if they are no longer in the remote
   system("git remote prune origin")
-  system("git branch --merged | grep -v \\* | xargs git branch -D")
+  Sys.sleep(.0005)
+  # list local branches
+  message("\nThe following branches are local:")
+  system("git branch -v")
+  Sys.sleep(.0005)
+  # list remote branches
+  message("\nThe following branches are remote:")
+  system("git branch -r")
+  Sys.sleep(.0005)
+  if(length(suppressWarnings(system("git branch --merged | grep -v \\*", intern = TRUE)))==0){
+    message("\nThere was nothing to clean up.")
+  } else {
+    message("\nCleaning up:")
+    # remove non-main local branches that have no commits
+    system("git branch --merged | grep -v \\* | xargs git branch -D")
+  }
 }
